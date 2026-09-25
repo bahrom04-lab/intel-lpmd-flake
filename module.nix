e96b0c15d199e3b173124aef6004728676b1340e
bahrom04 <magdiyevbahrom@gmail.com>
2026-09-25 17:31:51 +0500

    ignore process_cpuset files

--- module.nix
+++ module.nix
@@ -118,8 +118,8 @@ in
         "intel_lpmd/${configChoice.targetName}".source = configChoice.source;
 
         # https://github.com/intel/intel-lpmd/commit/14ae1eaf1ed005a490bff12a37c546a570fa3570
-        "intel_lpmd/process_cpuset.xml".source = "${pkg}/share/xml/process_cpuset.xml";
-        "intel_lpmd/process_cpuset_user.xml".source = "${pkg}/share/xml/process_cpuset_user.xml";
+        # "intel_lpmd/process_cpuset.xml".source = "${pkg}/share/xml/process_cpuset.xml";
+        # "intel_lpmd/process_cpuset_user.xml".source = "${pkg}/share/xml/process_cpuset_user.xml";
       };
 
     systemd.services.intel-lpmd = {
